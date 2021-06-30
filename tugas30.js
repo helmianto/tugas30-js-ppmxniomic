73af64daf766d0867524744659b10f5f61b8a9f0
Helmianto <helmianto15@gmail.com>
2021-06-30 16:57:42 +0700

    initial commit

---
+++ tugas30.js
@@ -0,0 +1,8 @@
+function regex(value){
+    let regEx = value.match(/[es]/g);
+    console.log(regEx);
+}
+
+regex(
+    "Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat"
+);
